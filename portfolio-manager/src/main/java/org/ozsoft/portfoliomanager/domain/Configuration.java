523ed4723593521096e49b98219cfbee0cb0dfe6
Oscar Stigter <oscar.stigter@asml.com>
2016-10-10 13:38:47 +0200

    Create daily backup of portfolio file.

--- portfolio-manager/src/main/java/org/ozsoft/portfoliomanager/domain/Configuration.java
+++ portfolio-manager/src/main/java/org/ozsoft/portfoliomanager/domain/Configuration.java
@@ -8,7 +8,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,8 +26,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.nio.file.Files;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
@@ -43,9 +47,9 @@ import com.google.gson.GsonBuilder;
 /**
  * The applcation's configuration. <br />
  * <br />
- * 
+ *
  * Implemented as singleton for easy use.
- * 
+ *
  * @author Oscar Stigter
  */
 public class Configuration {
@@ -59,6 +63,10 @@ public class Configuration {
     private static final File ANALYSIS_RESULT_FILE = new File(DATA_DIR, "stock_analysis.csv");
 
     private static final double INCOME_TAX_RATE = 0.15;
+
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    private static final int MAX_BACKUP_COUNT = 5;
 
     private static final Logger LOGGER = LogManager.getLogger(Configuration.class);
 
@@ -86,11 +94,12 @@ public class Configuration {
 
     /**
      * Returns the singleton instance.
-     * 
+     *
      * @return The singleton instance.
      */
     public static Configuration getInstance() {
         if (config == null) {
+            Configuration.createDailyBackup();
             config = Configuration.load();
         }
         return config;
@@ -98,7 +107,7 @@ public class Configuration {
 
     /**
      * Returns the latest downloaded CCC list file (Excel sheet), or {@code null} if not present (stock data has never been updated).
-     * 
+     *
      * @return The CCC list file.
      */
     public File getCCCListFile() {
@@ -107,7 +116,7 @@ public class Configuration {
 
     /**
      * Returns the latest generated analysis result file, or {@code null} if not present (analysis has never been run).
-     * 
+     *
      * @return The analysis result file.
      */
     public File getAnalysisResultFile() {
@@ -116,7 +125,7 @@ public class Configuration {
 
     /**
      * Returns all stocks, sorted by their name.
-     * 
+     *
      * @return All stocks.
      */
     public Set<Stock> getStocks() {
@@ -125,7 +134,7 @@ public class Configuration {
 
     /**
      * Returns all currently owned stocks (open postions).
-     * 
+     *
      * @return
      */
     public Set<Stock> getOwnedStocks() {
@@ -140,10 +149,10 @@ public class Configuration {
 
     /**
      * Returns a stock based on its symbol.
-     * 
+     *
      * @param symbol
      *            The stock's symbol.
-     * 
+     *
      * @return The stock if found, otherwise {@code null}.
      */
     public Stock getStock(String symbol) {
@@ -153,12 +162,12 @@ public class Configuration {
     /**
      * Adds a new stock. <br />
      * <br />
-     * 
+     *
      * Only adds a stock if is new, based on its symbol.
-     * 
+     *
      * @param stock
      *            The stock.
-     * 
+     *
      * @return {@code true} if the stock was added, otherwise {@code false}.
      */
     public boolean addStock(Stock stock) {
@@ -174,14 +183,13 @@ public class Configuration {
 
     /**
      * Permenently deletes a stock (including all data).
-     * 
+     *
      * @param stock
      *            The stock.
-     * 
+     *
      * @return {@code true} if the stock was deleted, otherwise {@code false}.
      */
     public boolean deleteStock(Stock stock) {
-        // TODO: Only allow deleting stocks that have never been owned (no open or closed positions).
         String symbol = stock.getSymbol();
         if (stocks.containsKey(symbol)) {
             stocks.remove(symbol);
@@ -194,7 +202,7 @@ public class Configuration {
 
     /**
      * Returns all transactions, sorted by date.
-     * 
+     *
      * @return The transactions.
      */
     public List<Transaction> getTransactions() {
@@ -212,7 +220,7 @@ public class Configuration {
 
     /**
      * Adds a transaction.
-     * 
+     *
      * @param transaction
      *            The transaction.
      */
@@ -222,7 +230,7 @@ public class Configuration {
 
     /**
      * Deletes a transaction.
-     * 
+     *
      * @param transaction
      *            The transaction.
      */
@@ -232,7 +240,7 @@ public class Configuration {
 
     /**
      * Returns the current stock portfolio.
-     * 
+     *
      * @return The portfolio.
      */
     public Portfolio getPortfolio() {
@@ -246,10 +254,10 @@ public class Configuration {
 
     /**
      * Returns whether a position (open or closed) exists for the specified stock.
-     * 
+     *
      * @param stock
      *            The stock.
-     * 
+     *
      * @return {@code true} is a position exists, otherwise {@code false}.
      */
     public boolean hasPosition(Stock stock) {
@@ -264,7 +272,7 @@ public class Configuration {
 
     /**
      * Returns whether to round monetary totals values (do not display cents).
-     * 
+     *
      * @return whether to round monetary totals
      */
     public boolean isRoundTotals() {
@@ -273,7 +281,7 @@ public class Configuration {
 
     /**
      * Sets whether to round monetary totals values (do not display cents).
-     * 
+     *
      * @param roundTotals
      *            {@code true} if values should be rounded, otherwise {@code false}.
      */
@@ -283,7 +291,7 @@ public class Configuration {
 
     /**
      * Returns whether to show closed (sold) positions.
-     * 
+     *
      * @return Whether to show closed positions.
      */
     public boolean getShowClosedPositions() {
@@ -292,7 +300,7 @@ public class Configuration {
 
     /**
      * Sets wether to show closed (sold) positions.
-     * 
+     *
      * @param showClosedPositions
      *            Whether to show closed positions.
      */
@@ -302,17 +310,17 @@ public class Configuration {
 
     /**
      * Returns the income tax rate.
-     * 
+     *
      * @return The income tax rate.
      */
     public static double getIncomeTaxRate() {
-        // TODO: Make income tax rate configurable.
+        // TODO: Make income tax rate configurable (globally and/or per stock).
         return INCOME_TAX_RATE;
     }
 
     /**
      * Returns whether to automatically deduct income tax from received income.
-     * 
+     *
      * @return Whether to deduct income tax.
      */
     public boolean isDeductIncomeTax() {
@@ -321,7 +329,7 @@ public class Configuration {
 
     /**
      * Sets whether to automatically deduct income tax from received income.
-     * 
+     *
      * @param deductIncomeTax
      *            Whether to deduct income tax.
      */
@@ -331,7 +339,7 @@ public class Configuration {
 
     /**
      * Loads the configuration from file.
-     * 
+     *
      * @return The configuration.
      */
     private static Configuration load() {
@@ -369,6 +377,42 @@ public class Configuration {
             LOGGER.debug("Configuration saved");
         } catch (IOException e) {
             LOGGER.error("Could not write data file: " + PORTFOLIO_FILE.getAbsolutePath(), e);
+        }
+    }
+
+    private static void createDailyBackup() {
+        if (PORTFOLIO_FILE.exists()) {
+            String timestamp = DATE_FORMAT.format(new Date());
+            String backupFileName = PORTFOLIO_FILE.getName() + "." + timestamp;
+            File backupFile = new File(DATA_DIR, backupFileName);
+            if (!backupFile.exists()) {
+                try {
+                    Files.copy(PORTFOLIO_FILE.toPath(), backupFile.toPath());
+                    LOGGER.debug("Daily backup created");
+                    cleanBackups();
+                } catch (IOException e) {
+                    LOGGER.error("Could not create backup of portfolio file", e);
+                }
+            }
+        }
+    }
+
+    private static void cleanBackups() {
+        Set<File> backupFiles = new TreeSet<File>();
+        for (File file : DATA_DIR.listFiles()) {
+            if (file.getName().startsWith(PORTFOLIO_FILE.getName() + ".")) {
+                backupFiles.add(file);
+            }
+        }
+
+        while (backupFiles.size() > MAX_BACKUP_COUNT) {
+            File file = backupFiles.iterator().next();
+            if (file.delete()) {
+                LOGGER.debug("Deleted old backup file: " + file.getName());
+            } else {
+                LOGGER.error("Could not delete old backup file: " + file.getName());
+            }
+            backupFiles.remove(file);
         }
     }
 }
